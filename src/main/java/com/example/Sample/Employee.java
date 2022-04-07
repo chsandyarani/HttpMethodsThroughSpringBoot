7cd1991b6df34f4e776be390419a6e02f9d35d91
Sandyarani Chinapuram <inschin@KMC.Corp>
2022-04-07 11:55:27 +0530

    commit

--- src/main/java/com/example/Sample/Employee.java
+++ src/main/java/com/example/Sample/Employee.java
@@ -1,13 +1,32 @@
 package com.example.Sample;
-
 public class Employee {
     String id;
     String name;
-
+//generating getters and setters
     public String getId() {
         return id;
     }
 
+    public String getName() {
+
+        return name;
+    }
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public void setId(String id) {
+
+        this.id = id;
+    }
+    //generating constructor
+    public Employee(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    //generating to-string
     @Override
     public String toString() {
         return "Employee{" +
@@ -16,20 +35,7 @@ public class Employee {
                 '}';
     }
 
-    public String getName() {
-        return name;
-    }
 
-    public Employee(String id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 
-    public void setId(String id) {
-        this.id = id;
-    }
 }
--- src/main/java/com/example/Sample/SampleController.java
+++ src/main/java/com/example/Sample/SampleController.java
@@ -1,5 +1,6 @@
 package com.example.Sample;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -7,22 +8,44 @@ import java.util.List;
 
 @RestController
 public class SampleController {
-
+    //listing the all employee details
     List list = new ArrayList<Employee>();
 
-    @PostMapping("/add")
+
+    //posting employee details
+    @PostMapping("/post")
     public void add(@RequestBody Employee employee) {
         list.add(employee);
     }
-
+    //retrieves employee detail
     @GetMapping("/get")
     public List<Employee> get() {
+
         return list;
     }
+    //Deleting specific record
+    @DeleteMapping(path = "/{id}")
+    public void delete(@PathVariable("id")  Employee id) {
+        {
+            list.clear();
+        }
+    }
 
-    @DeleteMapping("/del")
-    public List<Employee> employeeList() {
-        list.remove(employeeList())
 
-                
+
+//    @DeleteMapping("/del/{id}")
+//  public ResponseEntity<Employee> deleteEmployee(@PathVariable int id) {
+//  ` return new ResponseEntity("Employee Deleted", HttpStatus.OK);
+//
+//    }
+//
+//
+//   @DeleteMapping("/del/{id}")
+//       public void delete(@PathVariable("id") int id)
+//   {
+//
+//   }
+//   @PutMapping("/put/{id}")
+//    public void put()
+//}
 }
